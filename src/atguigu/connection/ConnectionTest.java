9b541915557ea8b7ad1e033b08926995f18478a5
Yajun Zhang <64558297+MikeZhangYajun@users.noreply.github.com>
2020-05-23 21:41:16 -0400

    minor changes

--- src/atguigu/connection/ConnectionTest.java
+++ src/atguigu/connection/ConnectionTest.java
@@ -15,53 +15,63 @@ public class ConnectionTest {
 	@Test
 	public void testConnection1() throws SQLException {
 		Driver driver = new com.mysql.jdbc.Driver();
-		String url = "jdbc:mysql://localhost:3306/vrg";
+		
+		String url = "jdbc:mysql://localhost:3306/test";
+		
 		Properties info = new Properties();
 		info.setProperty("user", "root");
-		info.setProperty("password", "32456Mysql");
+		info.setProperty("password", "abc123");
+		
 		Connection connect = driver.connect(url, info);
 		System.out.println(connect);
 	}
 	//对方式一的迭代：在如下的程序中不出现第三方的api，使得程序具有更好的可移植性
 	@Test
 	public void testConnection2() throws Exception {
-		Class clazz = Class.forName("com.mysql.jdbc.Driver");
+		Class<?> clazz = Class.forName("com.mysql.jdbc.Driver");
 		Driver driver = (Driver) clazz.newInstance();
+		
 		String url = "jdbc:mysql://localhost:3306/vrg";
+		
 		Properties info = new Properties();
 		info.setProperty("user", "root");
-		info.setProperty("password", "32456Mysql");
+		info.setProperty("password", "abc123");
+		
 		Connection connect = driver.connect(url, info);
 		System.out.println(connect);
 	}
 	//方式三：使用DriverManager替换Driver
 	@Test
 	public void testConnection3() throws Exception {
-		Class clazz = Class.forName("com.mysql.jdbc.Driver");
-		Driver driver = (Driver) clazz.newInstance();
 		String url = "jdbc:mysql://localhost:3306/vrg";
 		String user = "root";
-		String password = "32456Mysql";
+		String password = "abc123";
 		
+		Class<?> clazz = Class.forName("com.mysql.jdbc.Driver");
+		Driver driver = (Driver) clazz.newInstance();
 		DriverManager.registerDriver(driver);
+		
 		Connection connection = DriverManager.getConnection(url, user, password);
 		System.out.println(connection);
 	}
 	
-	//方式四： 可以只是加载驱动，不用显示地注册驱动
+	//方式四： 可以只是加载驱动，不用显式地注册驱动
 	@Test
 	public void testConnection4() throws Exception {
 		String url = "jdbc:mysql://localhost:3306/vrg";
 		String user = "root";
-		String password = "32456Mysql";
+		String password = "abc123";
 		
 		Class.forName("com.mysql.jdbc.Driver");
-		//省略注册环节，因为在mysql的Driver实现类中，声明了一个静待代码块
+		//省略注册环节，因为在mysql的Driver实现类中，声明了一个静待代码块，里面给做了注册
 //		Driver driver = (Driver) clazz.newInstance();		
 //		DriverManager.registerDriver(driver);
+		
 		Connection connection = DriverManager.getConnection(url, user, password);
 		System.out.println(connection);
 	}
+	
+	
 	//方式五(final版)：将数据库连接需要的四个基本信息声明在配置文件中，通过读取配置文件的方式，获取链接
 	/*
 	 * 1.实现了数据与代码的分离，实现了解耦
@@ -71,12 +81,13 @@ public class ConnectionTest {
 	public void testConnection5() throws Exception {
 		InputStream inputStream = ConnectionTest.class.getClassLoader().getResourceAsStream("jdbc.properties");
 		Properties pros = new Properties();
-		pros.load(inputStream);
+		pros.load(inputStream); 
 		String driverClass = pros.getProperty("driverClass");
 		String user = pros.getProperty("user");
 		String password = pros.getProperty("password");
 		String url = pros.getProperty("url");
 		Class.forName(driverClass);
+		
 		Connection connection = DriverManager.getConnection(url, user, password);
 		System.out.println(connection);
 		
